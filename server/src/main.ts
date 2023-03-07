8b49b41216c7929dc0dd5ae273df8f3ff3da85ed
Luigi Garcia <luigifgp@gmail.com>
2023-03-07 19:19:59 +0000

    fix(server): server Origin issue allow all origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+  await app.listen(process.env.PORT || 3000);
+  console.log(`server running on PORT ${process.env.PORT || 3000}`);
 }
 bootstrap();
