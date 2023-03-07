d8dcc6d0566cd790ba30d8fc1433ca0d9013c95d
Luigi Garcia <luigi@finaldream.de>
2023-03-07 17:56:10 +0000

    chore: adjust tailwind config & add colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //Background
+        default: '#F0F0F1',
+        box: '#000000',
+        primary: '#FBAC00',
+        secondary: '#F5F4F5',
+      },
+    },
   },
   plugins: [],
-}
+};
