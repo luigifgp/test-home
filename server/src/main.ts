c53f01d520017bc94668a55edc9ba94c7921b77a
Luigi Garcia <luigifgp@gmail.com>
2023-03-08 09:38:22 +0000

    chore: cors issue and build

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,9 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -14,7 +16,8 @@ async function bootstrap() {
 
   app.enableCors({
     allowedHeaders: '*',
-    origin: '*',
+    origin: ['https://client-test-home-luigi18.vercel.app', '*'],
+    credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
   console.log(`server running on PORT ${process.env.PORT || 3000}`);
